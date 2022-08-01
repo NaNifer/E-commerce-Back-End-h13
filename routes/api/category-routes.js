1f07020d4d8693d59c765134190d3846c1a560e8
Nifer Kilakila <nifercritter@gmail.com>
2022-07-31 21:42:29 -0700

    Started category routes, sketched out first 2 routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,17 +1,48 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { findAll } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    categories = await Category.findAll({
+      include: [
+        { model: Product }
+      ]
+    });
+    res.json(categories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 });
+
+// router.get('/:id', (req, res) => {
+//   // TODO: Add a comment describing the functionality of this method
+//   Book.findByPk(req.params.id).then((bookData) => {
+//     res.json(bookData);
+//   });
+// });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    category = await Category.findByPk(req.params.id, {
+      include: [
+        { model: Product }
+      ]
+    });
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
