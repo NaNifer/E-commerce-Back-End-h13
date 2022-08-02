907a2f1429a64bb61b542fb1b79767614073e3e5
Nifer Kilakila <nifercritter@gmail.com>
2022-08-01 23:03:27 -0700

    Sketched out update a tag's name by its uid=501(nifer) gid=20(staff) groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh),400(com.apple.access_remote_ae) value in tag-routes, and added async to functions

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,7 +4,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
   // find all tags and its associated Product data
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   try {
     tags = await Tag.findAll({
       include: [
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
 });
 
   // find a single tag by its `id` and its associated Product data
-  router.get('/:id', (req, res) => {
+  router.get('/:id', async(req, res) => {
   try {
     tag = await Tag.findByPk(req.params.id, {
       include: [
@@ -34,7 +34,7 @@ router.get('/', (req, res) => {
 });
 
   // create a new tag
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   try {
     const newTag = await Tag.create(req.body);
     res.json(newTag);
@@ -44,15 +44,29 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
+  try {
+    updatedTag = await Tag.update(
+      {
+        id: req.body.id,
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.json(updatedTag);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 });
 
-
-
-
   // delete on tag by its `id` value
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   try {
     deletedTag = await Tag.destroy({
       where: {
